61c05497e8bd88014cb71546fd28c40a65665e42
Michael Lauritsen <i30288741@live.dk>
2020-10-07 19:11:40 +0200

    added new user function

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,29 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const firebase = require('firebase');
+const app = require('express')();
+
+const Config = {
+  apiKey: 'AIzaSyAz0ykIL2gjxuta2D1ftKlMEQlhTrbLgNQ',
+  authDomain: 'bugtracker-8b0d5.firebaseapp.com',
+  databaseURL: 'https://bugtracker-8b0d5.firebaseio.com',
+  projectId: 'bugtracker-8b0d5',
+  storageBucket: 'bugtracker-8b0d5.appspot.com',
+  messagingSenderId: '716587226540',
+  appId: '1:716587226540:web:e2de17e6dbfe31bbde9609',
+  measurementId: 'G-2MD3NY3QYQ',
+};
+
+// Initialize Firebase
 
 admin.initializeApp();
+firebase.initializeApp(Config);
+//firebase.analytics();
 
-const express = require('express');
-const app = express();
+const db = admin.firestore();
 
 app.get('/bugs', (req, res) => {
-  admin
-    .firestore()
-    .collection('bugs')
+  db.collection('bugs')
     .orderBy('createdAt', 'desc')
     .get()
     .then(data => {
@@ -36,9 +50,7 @@ app.post('/bug', (req, res) => {
     createdAt: new Date().toISOString(),
   };
 
-  admin
-    .firestore()
-    .collection('bugs')
+  db.collection('bugs')
     .add(newBug)
     .then(doc => {
       res.json({ message: `document ${doc.id} added successfully` });
@@ -46,6 +58,55 @@ app.post('/bug', (req, res) => {
     .catch(error => {
       res.status(500).json({ error: 'Something went wrong' });
       console.log(error);
+    });
+});
+
+// Signup route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  //TODO validate data
+
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: 'this handle is taken' });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch(error => {
+      console.error(error);
+      if (error.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ message: 'Email is already registered' });
+      }
+      return res.status(500).json({ error: error.code });
     });
 });
 
